ecb348347073f82b8a2cad3785291b288eac4fa9
Alijan Beshimow <beshimowalijan@gmail.com>
2023-09-29 18:28:12 +0300

    changes

--- app.py
+++ app.py
@@ -1,31 +1,94 @@
-import pickle
+import sqlite3
 from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 
 app.secret_key = "12312323423wddasdsadasd"
 
-users = {"tal": "admin", "gal": "admin", "alex": "admin"}
+
+def initialize_database():
+    conn = sqlite3.connect('tasks.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            date TEXT,
+            category TEXT,
+            username TEXT NOT NULL,
+            notes TEXT
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
 
 
-def load():
-    try:
-        with open("tasks.pickle", 'rb') as file:
-            return pickle.load(file)
-    except FileNotFoundError:
-        return []
+def load_tasks(username):
+    conn = sqlite3.connect('tasks.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tasks WHERE username=?", (username,))
+    tasks = cursor.fetchall()
+    conn.close()
+    return tasks
 
 
-def save(tasks):
-    with open("tasks.pickle", 'wb') as file:
-        pickle.dump(tasks, file)
+def save_task(task):
+    conn = sqlite3.connect('tasks.db')
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO tasks (name, date, category, username, notes) VALUES (?, ?, ?, ?, ?)", task)
+    conn.commit()
+    conn.close()
+
+
+def authenticate_user(username, password):
+    conn = sqlite3.connect('tasks.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+    user = cursor.fetchone()
+    conn.close()
+
+    if user and user[2] == password:
+        return True
+    else:
+        return False
+
+
+def load_user_details(username):
+    conn = sqlite3.connect('tasks.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+    user_details = cursor.fetchone()
+    conn.close()
+    return user_details
+
+
+@app.route('/profile')
+def profile():
+    if session.get("username"):
+        username = session["username"]
+        user_details = load_user_details(username)
+        return render_template("profile.html", user=user_details)
+    else:
+        return redirect('/login')
 
 
 @app.route('/')
 def hello():
-    username = session.get("username")
-    if username in users:
-        return render_template('home.html')
+    if session.get("username"):
+        username = session["username"]
+        user_details = load_user_details(username)
+        return render_template("add.html", user=user_details)
     else:
         return redirect('/login')
 
@@ -33,7 +96,6 @@ def hello():
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if session.get("username"):
-        tasks = load()
         username = session["username"]
         if request.method == 'POST':
             name = request.form.get("name")
@@ -41,8 +103,7 @@ def add():
             category = request.form.get("category")
             notes = request.form.get("notes")
             new_task = (name, date, category, username, notes)
-            tasks.append(new_task)
-            save(tasks)
+            save_task(new_task)
             return redirect('/view')
         return render_template("add.html")
     else:
@@ -53,11 +114,7 @@ def add():
 def view():
     if session.get("username"):
         username = session["username"]
-        tasks = load()
-        user_tasks = []
-        for task in tasks:
-            if task[3] == username:
-                user_tasks.append(task)
+        user_tasks = load_tasks(username)
         return render_template("view.html", tasks=user_tasks)
     else:
         return redirect('/login')
@@ -65,43 +122,44 @@ def view():
 
 @app.route('/delete', methods=['POST'])
 def delete_task():
-    tasks = load()
-    task_index = int(request.form.get("task_index"))
-
-    if 0 <= task_index < len(tasks):
-        del tasks[task_index]
-        save(tasks)
-
+    task_id = int(request.form.get("task_id"))
+    conn = sqlite3.connect('tasks.db')
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM tasks WHERE id=?", (task_id,))
+    conn.commit()
+    conn.close()
     return redirect('/view')
 
 
 @app.route('/search')
 def search():
-    tasks = load()
-    search = request.args.get("search")
-    filtered_tasks = []
-    for task in tasks:
-        if search in task:
-            filtered_tasks.append(task)
-    if not filtered_tasks:
-        return render_template("error.html", message="Task not Found")
-    return render_template("view.html", tasks=filtered_tasks)
+    if session.get("username"):
+        username = session["username"]
+        search = request.args.get("search")
+        conn = sqlite3.connect('tasks.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM tasks WHERE (name LIKE ? OR notes LIKE ?) AND username = ?",
+                       ('%' + search + '%', '%' + search + '%', username))
+        filtered_tasks = cursor.fetchall()
+        conn.close()
+        if not filtered_tasks:
+            return render_template("error.html", message="Task not Found")
+        return render_template("view.html", tasks=filtered_tasks)
+    else:
+        return redirect('/login')
 
 
-@app.route('/update')
-def update():
-    return render_template("update.html")
-
-
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template("login.html")
     else:
-        if {request.form["username"] in users} and (request.form["password"] == "admin"):
-            session["username"] = request.form["username"]
+        username = request.form["username"]
+        password = request.form["password"]
+        if authenticate_user(username, password):
+            session["username"] = username
             return redirect('/')
-        return render_template("login.html", message="incorrect user or password")
+        return render_template("login.html", message="Error username or password")
 
 
 @app.route('/logout')
@@ -115,8 +173,59 @@ def register():
     if request.method == 'POST':
         username = request.form.get("username")
         password = request.form.get("password")
-        if username in users:
+        conn = sqlite3.connect('tasks.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+        existing_user = cursor.fetchone()
+        conn.close()
+
+        if existing_user:
             return render_template("register.html", message="Username already exists")
-        users[username] = password
+
+        conn = sqlite3.connect('tasks.db')
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        conn.commit()
+        conn.close()
+
         return redirect('/login')
+
     return render_template("register.html")
+
+
+@app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
+def edit(task_id):
+    if session.get("username"):
+        username = session["username"]
+
+        if request.method == 'POST':
+            name = request.form.get("name")
+            date = request.form.get("date")
+            category = request.form.get("category")
+            notes = request.form.get("notes")
+            conn = sqlite3.connect('tasks.db')
+            cursor = conn.cursor()
+            cursor.execute(
+                "UPDATE tasks SET name=?, date=?, category=?, notes=? WHERE id=? AND username=?",
+                (name, date, category, notes, task_id, username)
+            )
+            conn.commit()
+            conn.close()
+
+            return redirect('/view')
+
+        conn = sqlite3.connect('tasks.db')
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT * FROM tasks WHERE id=? AND username=?", (task_id, username))
+        task = cursor.fetchone()
+        conn.close()
+
+        if task:
+            return render_template("edit.html", task=task)
+        else:
+            return render_template("error.html", message="Task not found")
+
+    else:
+        return redirect('/login')
